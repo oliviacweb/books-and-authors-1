ff7b81807388efdcf9f39b0c7d50fc81e4b084d2
Olivia Webster <oliviacweb@gmail.com>
2020-01-05 13:45:53 -0700

    passed all tests

--- author.js
+++ author.js
@@ -1,5 +1,17 @@
-class Author {
+var Book = require('./book.js');
 
+class Author {
+  constructor({first, last}) {
+     this.first = first;
+     this.last = last;
+     this.books = [];
+  }
+ write(title, publicationYear) {
+   var book = new Book(this.first, this.last, title, publicationYear);
+   this.books.push(book);
+   return book;
+
+ }
 }
 
 module.exports = Author;
--- book.js
+++ book.js
@@ -1,4 +1,9 @@
 class Book {
+  constructor(first, last, title, publicationYear) {
+    this.title = title;
+    this.publicationYear = publicationYear;
+    this.author = first + ' ' + last;
+  }
 
 }
 
